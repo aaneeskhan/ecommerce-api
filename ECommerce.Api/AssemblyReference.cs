fb9b6923eb8c83ce22a467c0a10682dea70f4d9d
Anees Khan <mohammadaanees.01@gmail.com>
2026-02-23 11:49:57 +0530

    Initial commit

---
+++ ECommerce.Api/AssemblyReference.cs
@@ -0,0 +1,10 @@
+﻿namespace ECommerce.Api
+{
+    public static class AssemblyReference
+    {
+        public static IServiceCollection AddApi(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
---
+++ ECommerce.Persistence/AssemblyReference.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace ECommerce.Persistence
+{
+    public static class AssemblyReference
+    {
+        public static IServiceCollection AddApi(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
