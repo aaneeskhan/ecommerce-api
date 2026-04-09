dc5419e54d9f80c80adf2ec30b0b35ecf8edcb4b
Anees Khan <mohammadaanees.01@gmail.com>
2026-04-09 10:45:19 +0530

    Update UsersController.cs

--- ECommerce.Api/Controllers/UsersController.cs
+++ ECommerce.Api/Controllers/UsersController.cs
@@ -7,28 +7,26 @@ using Microsoft.AspNetCore.Mvc;
 namespace ECommerce.Api.Controllers
 {
     [Route("api/[controller]")]
+    [Authorize]
     [ApiController]
     public class UsersController(IUserService userService) : ControllerBase
     {
         [HttpGet("")]
-        [Authorize]
         public async Task<IResult> GetUsers() => this.ApiResponse(await userService.GetUsers());
 
         [HttpGet("{id:guid}")]
-        [Authorize]
         public async Task<IResult> GetUserById(Guid id) => this.ApiResponse(await userService.GetUserById(id));
 
+
          [HttpGet("role/{userRole}")]
-        [Authorize]
         public async Task<IResult> GetUserByRole(string userRole) => this.ApiResponse(await userService.GetUserByRole(userRole));
 
         [HttpGet("email/{email}")]
-        [Authorize]
         public async Task<IResult> GetUserByEmail(string email) => this.ApiResponse(await userService.GetUserByEmail(email));
 
-        [HttpPut("update/{userStatus,id}")]
-        [Authorize]
-        public async Task<IResult> GetUserByEmail(UserStatus userStatus,Guid id) => this.ApiResponse(await userService.UpdateUserStatus(userStatus,id));
+
+        [HttpPut("{userStatus}/{id}")]
+        public async Task<IResult> UpdateUserStatus(UserStatus userStatus,Guid id) => this.ApiResponse(await userService.UpdateUserStatus(userStatus,id));
 
 
 
